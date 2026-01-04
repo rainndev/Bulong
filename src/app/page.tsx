5a76f6e8f4cfcf09a8bc9e2449b59aeffa6ba829
rainndev <182114243+rainndev@users.noreply.github.com>
2026-01-04 13:32:12 +0800

    feat: create LandingPageNavBar component with navigation and branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,16 @@
 "use client";
 
+import LandingPageNavBar from "@/components/LandingPageNavBar";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <main className="flex h-screen items-center justify-center bg-neutral-950 text-white">
-      <div className="flex gap-4">
-        <button
-          onClick={() => router.push("/sign-up")}
-          className="rounded-md bg-white px-6 py-2 font-medium text-black hover:bg-gray-200"
-        >
-          Sign Up
-        </button>
-        <button
-          onClick={() => router.push("/sign-in")}
-          className="rounded-md border border-white px-6 py-2 font-medium text-white hover:bg-neutral-800"
-        >
-          Sign In
-        </button>
-      </div>
+    <main className="flex h-screen flex-col items-center justify-center gap-10 bg-violet-50 p-10 text-white">
+      <LandingPageNavBar />
+
+      <div className="flex gap-4"></div>
     </main>
   );
 }
---
+++ src/components/LandingPageNavBar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const LandingPageNavBar = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-full rounded-2xl bg-violet-400/10 px-10 py-5 text-[#242731] backdrop-blur-lg">
+      <ul className="flex items-center justify-between">
+        <li className="flex">
+          <Image
+            src={"/logo-transparent-1.png"}
+            alt="Bulong Logo"
+            width={60}
+            height={60}
+          />
+          <div>
+            <h1 className="text-4xl font-bold">Bulong</h1>
+            <p className="text-sm">Your trusted feedback companion</p>
+          </div>
+        </li>
+
+        <div className="flex gap-10 text-lg font-medium">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">About</li>
+          <li className="cursor-pointer">Contact</li>
+        </div>
+
+        <li className="flex gap-5">
+          <button
+            onClick={() => router.push("/sign-in")}
+            className="md:text-md w-full cursor-pointer rounded-2xl bg-violet-500 px-4 py-3 text-sm font-medium text-nowrap text-white transition-colors ease-in-out hover:bg-violet-400 md:p-4 md:px-10"
+          >
+            Log in
+          </button>
+          <button
+            onClick={() => router.push("/sign-up")}
+            className="cursor-pointer text-nowrap"
+          >
+            Get Started
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default LandingPageNavBar;
