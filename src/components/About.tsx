04e541576bae96ce944c544631391e5361bcfa63
rainndev <182114243+rainndev@users.noreply.github.com>
2026-01-10 11:12:29 +0800

    feat: add bulong-peeking image and enhance About component styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
   return (
     <div
       id="about"
-      className="flex h-screen flex-col justify-center gap-6 rounded-2xl border bg-linear-to-b from-violet-200 via-violet-50 to-transparent p-4 ring-2 ring-violet-200 ring-offset-7 ring-offset-violet-50 backdrop-blur-lg md:rounded-3xl md:p-10 lg:rounded-4xl lg:p-20"
+      className="relative flex h-screen flex-col justify-center gap-6 overflow-hidden rounded-2xl border p-4 ring-2 ring-violet-200 ring-offset-7 ring-offset-violet-50 backdrop-blur-lg [background:radial-gradient(125%_125%_at_50%_10%,transparent_40%,#63e_100%)] md:rounded-3xl md:p-10 lg:rounded-4xl lg:p-20"
     >
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] mask-[radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] bg-size-[24px_24px] md:bg-size-[30px_30px] lg:bg-size-[40px_40px]"></div>
+
+      <Image
+        src={"/bulong-peeking.png"}
+        alt="Bulong Peeking"
+        width={200}
+        height={200}
+        className="absolute -bottom-10 -left-12 mx-auto md:size-48 lg:-bottom-20 lg:-left-20 lg:size-80"
+      />
       <h1 className="text-center text-[clamp(2rem,5vw,96rem)] font-bold text-pretty text-[#242731] antialiased md:mt-20">
         About
       </h1>
