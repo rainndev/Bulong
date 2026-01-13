c9371c62050b91c8e0039bbb9250c1ef64f4ed3c
rainndev <182114243+rainndev@users.noreply.github.com>
2026-01-13 23:30:04 +0800

    feat: enhance settings page layout and integrate SideBar component

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,4 @@
+import SideBar from "@/components/SideBar";
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { headers } from "next/headers";
@@ -25,26 +26,33 @@ const page = async () => {
   };
 
   return (
-    <div className="flex h-dvh flex-col items-center justify-center gap-6 p-4">
-      <h1 className="text-4xl font-bold">Settings Page</h1>
-      <form action={saveSettings}>
-        <div>
-          <p className="">Limit Message</p>
-          <input
-            type="number"
-            name="dailyLimit"
-            className="no-spinner md:text-md mt-5 w-full rounded-2xl border-2 border-violet-300 p-3 pl-4 text-sm transition-colors duration-200 focus:border-violet-500 focus:ring-2 focus:ring-violet-200 focus:ring-offset-2 focus:ring-offset-white focus:outline-none md:p-5"
-            placeholder="ex: 10"
-          />
-        </div>
+    <div className="flex h-dvh w-full bg-violet-50">
+      <SideBar currentPath="/settings" />
 
-        <button
-          type="submit"
-          className="md:text-md mt-5 flex w-full cursor-pointer justify-center rounded-2xl bg-violet-500 px-4 py-3 text-sm font-medium text-white transition-colors ease-in-out hover:bg-violet-400 md:p-5"
-        >
-          Save Settings
-        </button>
-      </form>
+      <div className="flex w-full flex-col gap-6 p-5 md:p-10 lg:p-20">
+        <h1 className="text-[clamp(1.25rem,2vw,2.25rem)] font-bold">
+          Settings Page
+        </h1>
+
+        <form action={saveSettings}>
+          <div className="flex flex-col gap-4 rounded-2xl bg-violet-100 p-5 md:gap-6 md:p-7 lg:p-10">
+            <p className="text-[clamp(1rem,2vw,1.125rem)]">Limit Message</p>
+            <input
+              type="number"
+              name="dailyLimit"
+              className="no-spinner md:text-md w-full rounded-2xl border-2 border-violet-300 p-3 pl-4 text-sm transition-colors duration-200 focus:border-violet-500 focus:ring-2 focus:ring-violet-200 focus:ring-offset-2 focus:ring-offset-white focus:outline-none md:p-5"
+              placeholder="ex: 10"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="md:text-md mt-5 flex w-full cursor-pointer justify-center rounded-2xl bg-violet-500 px-4 py-3 text-sm font-medium text-white transition-colors ease-in-out hover:bg-violet-400 md:p-5"
+          >
+            Save Settings
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -15,7 +15,7 @@ const links = [
 ];
 
 interface SideBarProps {
-  currentPath: string;
+  currentPath: "/dashboard" | "/messages" | "/settings" | "/";
 }
 
 const SideBar = ({ currentPath = "/dashboard" }: SideBarProps) => {
